6075fa8c059d125fa0d11b28bf571e8a90048596
archylex <archylex@protonmail.com>
2020-10-12 19:02:03 +0300

    fix: tasks

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,10 +1,10 @@
 const router = require('express').Router({ mergeParams: true });
 const Task = require('./task.model');
-const boardService = require('./task.service');
+const taskService = require('./task.service');
 
 router.route('/').get(async (req, res, next) => {
   try {
-    const boards = await boardService.getAll(req.params.boardId);
+    const boards = await taskService.getAll(req.params.boardId);
     await res.json(boards);
   } catch (e) {
     return next(e);
@@ -13,7 +13,7 @@ router.route('/').get(async (req, res, next) => {
 
 router.route('/:id').get(async (req, res, next) => {
   try {
-    const board = await boardService.get(req.params.boardId, req.params.id);
+    const board = await taskService.get(req.params.boardId, req.params.id);
     res.status(200).send(board);
   } catch (e) {
     return next(e);
@@ -22,8 +22,11 @@ router.route('/:id').get(async (req, res, next) => {
 
 router.route('/').post(async (req, res, next) => {
   try {
-    const board = await boardService.save(
-      new Task({ ...req.body, boardId: req.params.boardId })
+    const board = await taskService.save(
+      new Task({
+        ...req.body,
+        boardId: req.params.boardId
+      })
     );
     res.status(200).send(board);
   } catch (e) {
@@ -33,7 +36,7 @@ router.route('/').post(async (req, res, next) => {
 
 router.route('/:id').put(async (req, res, next) => {
   try {
-    const board = await boardService.update(
+    const board = await taskService.update(
       new Task({
         ...req.body,
         id: req.params.id,
@@ -48,7 +51,7 @@ router.route('/:id').put(async (req, res, next) => {
 
 router.route('/:id').delete(async (req, res, next) => {
   try {
-    await boardService.remove(req.params.boardId, req.params.id);
+    await taskService.remove(req.params.boardId, req.params.id);
     res.sendStatus(204);
   } catch (e) {
     return next(e);
